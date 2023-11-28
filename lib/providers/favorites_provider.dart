c0ac236a57db2db02183ffeda31f6c6cf2490bc5
munsra <munsra@gmail.com>
2023-11-28 22:05:19 +0100

    Added meals and favorite providers.

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>((ref) {
+  return FavouriteMealsNotifier();
+});
